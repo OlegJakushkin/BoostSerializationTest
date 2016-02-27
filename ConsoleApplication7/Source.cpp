3826d2db152577f21417bfb29c9eeb6ddcd7a491
OlegJakushkin <oleg.jakushkin@gmail.com>
2016-02-27 12:07:04 +0300

    Vector serialization
    We can now read a vector of Pandas!

--- ConsoleApplication7/Source.cpp
+++ ConsoleApplication7/Source.cpp
@@ -4,7 +4,9 @@
 #include <boost/serialization/serialization.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
 #include <string>
+#include <vector>
 #include <fstream>
 using namespace std;
 
@@ -21,21 +23,29 @@ struct Panda {
 };
 
 int main() {
-	Panda p;
-	p.w = 666;
-	p.name = "Ole";
+	//Panda p, q;
+	//p.w = 666;
+	//p.name = "Ole";
+
+	//q.w = 333;
+	//q.name = "Bla";
+
+	vector<Panda> pan;
+	//pan.push_back(p);
+	//pan.push_back(q);
 
 	//ofstream out("out.txt"); 
 	//boost::archive::text_oarchive out_archive(out);
 
-	//out_archive << p;
+	//out_archive << pan;
 
 	ifstream in("out.txt"); 
 	boost::archive::text_iarchive in_archive(in);
 
-	in_archive >> p;
-
-	cout << p.w << " " << p.name;
+	in_archive >> pan;
+	for(auto & e : pan) {
+		cout << e.w << " " <<  e.name << endl;
+	}
 	cin.get();
 	return 0;
 }
